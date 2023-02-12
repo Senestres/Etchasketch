e5b640f18636a315b5ef34f32938cb149821a4c7
Senestres <100228689+Senestres@users.noreply.github.com>
2023-02-12 22:29:20 +0100

    Add colorpicker, eraser

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-let penColor = "purple"
+let penColor = "black"
 let gridSize = 50
 let grid = document.getElementById("grid")
+
+let tool
 
 //slider
 let slider = document.getElementById("slider");
 let output = document.getElementById("showValue");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = "Canvas size:" + slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  output.innerHTML = "Size:" + this.value;
   updateGrid()
 }
 
+//Grid creation and update
 function gridResize() {
+    gridSize = slider.value
     let gridPixelNumber = (1000 / gridSize)
     document.documentElement.style.setProperty("--size", gridPixelNumber + "px");
     document.documentElement.style.setProperty("--number", gridSize);
@@ -29,23 +32,51 @@ function createGrid() {
 }
 
 function updateGrid() {
-    gridSize = slider.value
     document.getElementById("grid").innerHTML = ""
     gridResize()
     createGrid()
-    console.log(gridSize, gridPixelNumber)
 }
 
+//Add hover to grid
 function addHover() {
     let gridArray = document.querySelectorAll(".gridSquare")
     gridArray.forEach( (e) => {
         e.addEventListener("mousemove", (event) => {
             if (event.buttons === 1) {
-            event.target.style.backgroundColor = penColor;
+                if (tool == "eraser") {
+                    event.target.style.backgroundColor = "white";
+
+                } else if (tool === "rainbow") {
+                    event.target.style.backgroundColor = randomRgbColor();
+
+                } else {
+                    event.target.style.backgroundColor = penColor;
+                }
+
             }
         })
     })
 }
 
-window.onload = createGrid()
-console.log(gridSize, gridPixelNumber)
+// radiobuttons
+function update() {
+    tool = document.querySelector('input[name="tool"]:checked').value;
+    console.log(tool)
+}
+
+// Create a random color
+function randomRgbColor() {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    return "rgb("+r+", "+g+", "+b+")";
+}
+
+// pick a color
+let colorPicker = document.getElementById("colorPicker")
+colorPicker.addEventListener("change", watchColorPicker, false);
+function watchColorPicker(event) {
+    penColor = event.target.value;
+}
+
+window.onload = updateGrid()
